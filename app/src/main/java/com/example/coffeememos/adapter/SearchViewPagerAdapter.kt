f68b44aabcb702fdd5497266d9d872c25c2905c8
sin0713 <narisin1124@gmail.com>
2022-11-16 12:52:57 +0900

    [Add]検索画面にViewPager追加

--- app/src/main/java/com/example/coffeememos/adapter/SearchViewPagerAdapter.kt
+++ app/src/main/java/com/example/coffeememos/adapter/SearchViewPagerAdapter.kt
@@ -3,7 +3,7 @@ package com.example.coffeememos.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class HomeViewPagerAdapter(parent: Fragment, private val fragmentList: List<Fragment>) : FragmentStateAdapter(parent){
+class SearchViewPagerAdapter(parent: Fragment, private val fragmentList: List<Fragment>) : FragmentStateAdapter(parent){
     override fun getItemCount(): Int {
         return fragmentList.size
     }
