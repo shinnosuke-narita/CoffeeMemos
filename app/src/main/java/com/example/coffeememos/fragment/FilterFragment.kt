44e631c9a222ff2564ba5ce8e910abc44efcf86a
sin0713 <narisin1124@gmail.com>
2022-12-04 22:35:47 +0900

    [Add]Roast, GrindSizeのロジック追加

--- app/src/main/java/com/example/coffeememos/fragment/FilterFragment.kt
+++ app/src/main/java/com/example/coffeememos/fragment/FilterFragment.kt
@@ -10,9 +10,9 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.coffeememos.Constants
 import com.example.coffeememos.R
 import com.example.coffeememos.databinding.FragmentFilterBinding
 import com.example.coffeememos.manager.SearchFilterManager
@@ -31,6 +31,10 @@ class FilterFragment : Fragment() {
 
     private lateinit var filterManager: SearchFilterManager
 
+    private lateinit var ratingRadioBtnList: List<ImageView>
+    private val roastViewList: MutableList<View> = mutableListOf()
+    private val grindSizeViewList: MutableList<View> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -44,11 +48,79 @@ class FilterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFilterBinding.inflate(inflater, container, false)
+
+        binding.ratingContainer.root.tag = requireActivity().getString(R.string.review)
+        ratingRadioBtnList = listOf(
+            binding.ratingContainer.radioBtnFirst,
+            binding.ratingContainer.radioBtnSecond,
+            binding.ratingContainer.radioBtnThird,
+            binding.ratingContainer.radioBtnFourth,
+            binding.ratingContainer.radioBtnFifth)
+
+        setUpRadioBtnContainer(Constants.roastList, binding.roastContainer, roastViewList) { index ->
+            viewModel.setRoastRadioBtnState(index)
+        }
+
+        setUpRadioBtnContainer(Constants.grindSizeList, binding.grindSizeContainer, grindSizeViewList) { index ->
+            viewModel.setGrindSizeRadioBtnState(index)
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.roastBtnStateList.observe(viewLifecycleOwner) { list ->
+            for ((i, isSelected) in list.withIndex()) {
+                val radioBtn = roastViewList[i].findViewById<ImageView>(R.id.radioBtn)
+                if (isSelected) radioBtn.setImageResource(R.drawable.ic_baseline_radio_button_checked_24)
+                else radioBtn.setImageResource(R.drawable.ic_baseline_radio_button_unchecked_24)
+            }
+        }
+
+        viewModel.selectedRoastText.observe(viewLifecycleOwner) { selectedText ->
+            if (selectedText.isEmpty()) {
+                binding.selectedRoast.visibility = View.GONE
+                return@observe
+            }
+
+            binding.selectedRoast.visibility = View.VISIBLE
+            binding.selectedRoast.text = selectedText
+        }
+
+        viewModel.roastMenuState.observe(viewLifecycleOwner) { state ->
+            if (state == null) return@observe
+
+            if (state == MenuState.OPEN)  expandMenu(binding.roastContainer)
+            else collapseMenu(binding.roastContainer)
+        }
+
+        viewModel.grindSizeBtnStateList.observe(viewLifecycleOwner) { list ->
+            for ((i, isSelected) in list.withIndex()) {
+                val radioBtn = grindSizeViewList[i].findViewById<ImageView>(R.id.radioBtn)
+                if (isSelected) radioBtn.setImageResource(R.drawable.ic_baseline_radio_button_checked_24)
+                else radioBtn.setImageResource(R.drawable.ic_baseline_radio_button_unchecked_24)
+            }
+        }
+
+        viewModel.selectedGrindSizeText.observe(viewLifecycleOwner) { selectedText ->
+            if (selectedText.isEmpty()) {
+                binding.selectedGrindSize.visibility = View.GONE
+                return@observe
+            }
+
+            binding.selectedGrindSize.visibility = View.VISIBLE
+            binding.selectedGrindSize.text = selectedText
+        }
+
+        viewModel.grindSizeMenuState.observe(viewLifecycleOwner) { state ->
+            if (state == null) return@observe
+
+            if (state == MenuState.OPEN)  expandMenu(binding.grindSizeContainer)
+            else collapseMenu(binding.grindSizeContainer)
+        }
+
 
         viewModel.countryMenuState.observe(viewLifecycleOwner) { state ->
             if (state == null) return@observe
@@ -79,12 +151,38 @@ class FilterFragment : Fragment() {
                 collapseMenu(binding.toolContainer, binding.toolFilterElements)
             }
         }
-//        viewModel.ratingMenuState.observe(viewLifecycleOwner) { state ->
-//            if (state == null) return@observe
-//
-//            if (state == MenuState.OPEN) expandMenu(R.layout.filter_radio_button_layout, binding.ratingContainer)
-//            else collapseMenu(binding.ratingContainer)
-//        }
+        viewModel.ratingMenuState.observe(viewLifecycleOwner) { state ->
+            if (state == null) return@observe
+
+            if (state == MenuState.OPEN) {
+                expandMenu(binding.ratingContainer.root)
+            } else {
+                collapseMenu(binding.ratingContainer.root)
+            }
+        }
+
+        viewModel.ratingRadioBtnState.observe(viewLifecycleOwner) { stateList ->
+            for ((i, isClicked) in stateList.withIndex()) {
+                if (isClicked) ratingRadioBtnList[i].setImageResource(R.drawable.ic_baseline_radio_button_checked_24)
+                else ratingRadioBtnList[i].setImageResource(R.drawable.ic_baseline_radio_button_unchecked_24)
+            }
+        }
+
+        viewModel.selectedRatingText.observe(viewLifecycleOwner) { text ->
+            if (text.isEmpty()) {
+                binding.selectedRatingTxt.visibility = View.GONE
+                return@observe
+            }
+
+            binding.selectedRatingTxt.visibility = View.VISIBLE
+            binding.selectedRatingTxt.text = text
+        }
+
+        for ((i, btn) in ratingRadioBtnList.withIndex()) {
+            btn.setOnClickListener {
+                viewModel.setRatingRadioBtnState(i)
+            }
+        }
 //        viewModel.sourMenuState.observe(viewLifecycleOwner) { state ->
 //            if (state == null) return@observe
 //
@@ -116,6 +214,24 @@ class FilterFragment : Fragment() {
 //            else collapseMenu(binding.richContainer)
 //        }
 
+        binding.roastWrapper.setOnClickListener {
+            val currentState = viewModel.roastMenuState.value ?: MenuState.CLOSE
+
+            if (currentState == MenuState.OPEN) viewModel.setRoastState(MenuState.CLOSE)
+            else viewModel.setRoastState(MenuState.OPEN)
+
+            viewModel.updateMenuState(binding.roastContainer, requireActivity())
+        }
+
+        binding.grindSizeWrapper.setOnClickListener {
+            val currentState = viewModel.grindSizeMenuState.value ?: MenuState.CLOSE
+
+            if (currentState == MenuState.OPEN) viewModel.setGrindSizeState(MenuState.CLOSE)
+            else viewModel.setGrindSizeState(MenuState.OPEN)
+
+            viewModel.updateMenuState(binding.grindSizeContainer, requireActivity())
+        }
+
 
         binding.countryWrapper.setOnClickListener {
             val currentState = viewModel.countryMenuState.value ?: MenuState.CLOSE
@@ -139,7 +255,7 @@ class FilterFragment : Fragment() {
             if (currentState == MenuState.OPEN) viewModel.setRatingState(MenuState.CLOSE)
             else viewModel.setRatingState(MenuState.OPEN)
 
-            viewModel.updateMenuState(binding.ratingContainer, requireActivity())
+            viewModel.updateMenuState(binding.ratingContainer.root, requireActivity())
         }
         binding.sourWrapper.setOnClickListener {
             val currentState = viewModel.sourMenuState.value ?: MenuState.CLOSE
@@ -211,7 +327,9 @@ class FilterFragment : Fragment() {
         // 閉じる処理
         binding.closeIcon.setOnClickListener {
             //parentViewModel.filterSearchResult()
+            parentViewModel.changeBottomSheetState()
             parentFragmentManager.popBackStack()
+
         }
     }
 
@@ -246,10 +364,23 @@ class FilterFragment : Fragment() {
         ValueAnimator.ofInt(parentView.height, 0).apply {
             duration = 300
             addUpdateListener {
-
                 if (it.animatedValue == 0) {
                     parentView.visibility = View.GONE
                     filterElementView.removeAllViews()
+                }
+                parentView.layoutParams.height = it.animatedValue as Int
+                parentView.requestLayout()
+            }
+            start()
+        }
+    }
+
+    private fun collapseMenu(parentView: ViewGroup) {
+        ValueAnimator.ofInt(parentView.height, 0).apply {
+            duration = 300
+            addUpdateListener {
+                if (it.animatedValue == 0) {
+                    parentView.visibility = View.GONE
                 }
                 parentView.layoutParams.height = it.animatedValue as Int
                 parentView.requestLayout()
@@ -334,5 +465,18 @@ class FilterFragment : Fragment() {
         newLinearLayout.orientation = LinearLayout.HORIZONTAL
         newLinearLayout.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         return newLinearLayout
+    }
+
+    private fun setUpRadioBtnContainer(dataList: List<String>, containerView: ViewGroup, viewList: MutableList<View>, clickListenerProcess:(Int) -> Unit ) {
+        for ((i, data) in dataList.withIndex()) {
+            val itemView = layoutInflater.inflate(R.layout.radio_btn_item, null)
+            itemView.findViewById<TextView>(R.id.name).text = data
+            itemView.setOnClickListener {
+                clickListenerProcess(i)
+            }
+            containerView.addView(itemView)
+
+            viewList.add(itemView)
+        }
     }
 }
--- app/src/main/java/com/example/coffeememos/viewModel/FilterViewModel.kt
+++ app/src/main/java/com/example/coffeememos/viewModel/FilterViewModel.kt
@@ -1,17 +1,30 @@
 package com.example.coffeememos.viewModel
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import com.example.coffeememos.Constants
 import com.example.coffeememos.R
-import com.example.coffeememos.search.FilterMenuInfo
 import com.example.coffeememos.state.MenuState
 
 class FilterViewModel : ViewModel() {
+    private val _roastMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val roastMenuState: LiveData<MenuState> = _roastMenuState
+
+    fun setRoastState(isOpen: MenuState) {
+        _roastMenuState.value = isOpen
+    }
+
+    private val _grindSizeMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val grindSizeMenuState: LiveData<MenuState> = _grindSizeMenuState
+
+    fun setGrindSizeState(isOpen: MenuState) {
+        _grindSizeMenuState.value = isOpen
+    }
+
     private val _countryMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
     val countryMenuState: LiveData<MenuState> = _countryMenuState
 
@@ -68,6 +81,69 @@ class FilterViewModel : ViewModel() {
         _richMenuState.value = isOpen
     }
 
+    private val _ratingRadioBtnState: MutableLiveData<MutableList<Boolean>> = MutableLiveData(MutableList(5) {false})
+    val ratingRadioBtnState: LiveData<MutableList<Boolean>> = _ratingRadioBtnState
+
+    val selectedRatingText: LiveData<String> = _ratingRadioBtnState.map { list ->
+        buildSelectedText(list) { index -> "${index + 1}.0,  " }
+    }
+
+    private fun buildSelectedText(list: MutableList<Boolean>, processData: (Int) -> String): String {
+        var resultText = ""
+        for ((i, isSelected) in list.withIndex()) {
+            if (isSelected) {
+                resultText += processData(i)
+            }
+        }
+        // 未選択の場合、空文字列を返す
+        if (resultText.isEmpty()) return resultText
+
+        val subStrLastIndex = resultText.lastIndexOf(",")
+        resultText = resultText.substring(0, subStrLastIndex)
+        return resultText
+    }
+
+    fun setRatingRadioBtnState(selectedIndex: Int) {
+        _ratingRadioBtnState.value = updateList(selectedIndex, _ratingRadioBtnState.value!!)
+    }
+
+    private val  _roastBtnStateList: MutableLiveData<MutableList<Boolean>> =
+        MutableLiveData(MutableList(Constants.roastList.size) {false})
+    val roastBtnStateList: LiveData<MutableList<Boolean>> = _roastBtnStateList
+
+    val selectedRoastText: LiveData<String> = _roastBtnStateList.map { list ->
+        buildSelectedText(list) { index -> "${Constants.roastList[index]},  "}
+    }
+
+    fun setRoastRadioBtnState(selectedIndex: Int) {
+        _roastBtnStateList.value = updateList(selectedIndex, _roastBtnStateList.value!!)
+    }
+
+    private val  _grindSizeBtnStateList: MutableLiveData<MutableList<Boolean>> =
+        MutableLiveData(MutableList(Constants.grindSizeList.size) {false})
+    val grindSizeBtnStateList: LiveData<MutableList<Boolean>> = _grindSizeBtnStateList
+
+    val selectedGrindSizeText: LiveData<String> = _grindSizeBtnStateList.map { list ->
+        buildSelectedText(list) { index ->
+            "${Constants.grindSizeList[index]},  "
+        }
+    }
+
+    fun setGrindSizeRadioBtnState(selectedIndex: Int) {
+        _grindSizeBtnStateList.value = updateList(selectedIndex, _grindSizeBtnStateList.value!!)
+    }
+
+
+    private fun updateList(selectedIndex: Int, currentList: MutableList<Boolean>): MutableList<Boolean> {
+        val updatedList = MutableList(currentList.size) { i -> currentList[i]}
+
+        for ((i, state) in currentList.withIndex()) {
+            if (i != selectedIndex) continue
+
+            updatedList[i] = !state
+        }
+        return updatedList
+    }
 
     // 現在開いているメニューのタグを保持する
     private var currentOpenViewTag: String? = null
@@ -83,6 +159,8 @@ class FilterViewModel : ViewModel() {
         } else {
             // 現在開かれているViewとは別のViewがクリックされたとき
             when (currentOpenViewTag) {
+                context.getString(R.string.roast) -> _roastMenuState.value = MenuState.CLOSE
+                context.getString(R.string.grind_size) -> _grindSizeMenuState.value = MenuState.CLOSE
                 context.getString(R.string.country) -> _countryMenuState.value = MenuState.CLOSE
                 context.getString(R.string.tool)   -> _toolMenuState.value = MenuState.CLOSE
                 context.getString(R.string.review) -> _ratingMenuState.value = MenuState.CLOSE
