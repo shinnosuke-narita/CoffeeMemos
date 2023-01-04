e320930c7a17faa916c732ff65471f76131fa04e
sin0713 <narisin1124@gmail.com>
2023-01-04 13:44:44 +0900

    [Fix]BeanFilterViewModel 同じ処理同士でまとめる

--- app/src/main/java/com/example/coffeememos/viewModel/BeanFilterViewModel.kt
+++ app/src/main/java/com/example/coffeememos/viewModel/BeanFilterViewModel.kt
@@ -11,171 +11,56 @@ import com.example.coffeememos.search.BeanFilterManager
 import com.example.coffeememos.state.MenuState
 
 class BeanFilterViewModel : BaseFilterViewModel() {
+    // メニュー開閉状態管理
     private val _countryMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
     val countryMenuState: LiveData<MenuState> = _countryMenuState
-
-    private val _countryValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
-    val countryValues: LiveData<List<String>> = _countryValues
-
-    val inputCountriesText: LiveData<String> = _countryValues.map { list ->
-         if (list.isEmpty())  return@map ""
-
-        return@map list.joinToString(", ")
-    }
-
-    fun setCountryMenuState(state: MenuState) {
-        _countryMenuState.value = state
-    }
-
-    fun addCountryValue(country: String) {
-        _countryValues.value = addList(country, _countryValues.value!!)
-    }
-    fun removeCountryValue(country: String) {
-        _countryValues.value = removeList(country, _countryValues.value!!)
-    }
 
     private val _farmMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
     val farmMenuState: LiveData<MenuState> = _farmMenuState
 
-    private val _farmValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
-    val farmValues: LiveData<List<String>> = _farmValues
+    private val _districtMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val districtMenuState: LiveData<MenuState> = _districtMenuState
 
-    val inputFarmText: LiveData<String> = _farmValues.map { list ->
-        if (list.isEmpty())  return@map ""
+    private val _storeMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val storeMenuState: LiveData<MenuState> = _storeMenuState
 
-        return@map list.joinToString(", ")
+    private val _processMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val processMenuState: LiveData<MenuState> = _processMenuState
+
+    private val _speciesMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val speciesMenuState: LiveData<MenuState> = _speciesMenuState
+
+    private val _ratingMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
+    val ratingMenuState: LiveData<MenuState> = _ratingMenuState
+
+    fun setCountryMenuState(state: MenuState) {
+        _countryMenuState.value = state
     }
 
     fun setFarmMenuState(state: MenuState) {
         _farmMenuState.value = state
     }
 
-    fun addFarmValue(farm: String) {
-        _farmValues.value = addList(farm, _farmValues.value!!)
-    }
-    fun removeFarmValue(farm: String) {
-        _farmValues.value = removeList(farm, _farmValues.value!!)
-    }
-
-    private val _districtMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
-    val districtMenuState: LiveData<MenuState> = _districtMenuState
-
-    private val _districtValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
-    val districtValues: LiveData<List<String>> = _districtValues
-
-    val inputDistrictText: LiveData<String> = _districtValues.map { list ->
-         if (list.isEmpty())  return@map ""
-
-        return@map list.joinToString(", ")
-    }
-
     fun setDistrictMenuState(state: MenuState) {
         _districtMenuState.value = state
-    }
-
-    fun addDistrictValue(district: String) {
-        _districtValues.value = addList(district, _districtValues.value!!)
-    }
-    fun removeDistrictValue(district: String) {
-        _districtValues.value = removeList(district, _districtValues.value!!)
-    }
-
-    private val _storeMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
-    val storeMenuState: LiveData<MenuState> = _storeMenuState
-
-    private val _storeValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
-    val storeValues: LiveData<List<String>> = _storeValues
-
-    val inputStoreText: LiveData<String> = _storeValues.map { list ->
-         if (list.isEmpty())  return@map ""
-
-        return@map list.joinToString(", ")
     }
 
     fun setStoreMenuState(state: MenuState) {
         _storeMenuState.value = state
     }
 
-    fun addStoreValue(store: String) {
-        _storeValues.value = addList(store, _storeValues.value!!)
-    }
-    fun removeStoreValue(store: String) {
-        _storeValues.value = removeList(store, _storeValues.value!!)
-    }
-
-    private val _processMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
-    val processMenuState: LiveData<MenuState> = _processMenuState
-
-    private val _processBtnStateList: MutableLiveData<MutableList<Boolean>> = MutableLiveData(MutableList(Constants.processList.size) { false })
-    val processBtnStateList: LiveData<MutableList<Boolean>> = _processBtnStateList
-
-    val selectedProcessText: LiveData<String> = _processBtnStateList.map { list ->
-        buildSelectedText(list) { index -> "${Constants.processList[index]},  " }
+    fun setSpeciesMenuState(state: MenuState) {
+        _speciesMenuState.value = state
     }
 
     fun setProcessMenuState(state: MenuState) {
         _processMenuState.value = state
     }
 
-    fun setProcessBtnState(selectedIndex: Int) {
-        _processBtnStateList.value = updateBtnStateList(selectedIndex, _processBtnStateList.value!!)
-    }
-
-    private val _speciesMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
-    val speciesMenuState: LiveData<MenuState> = _speciesMenuState
-
-    private val _speciesValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
-    val speciesValues: LiveData<List<String>> = _speciesValues
-
-    val inputSpeciesText: LiveData<String> = _speciesValues.map { list ->
-         if (list.isEmpty())  return@map ""
-
-        return@map list.joinToString(", ")
-    }
-
-    fun setSpeciesMenuState(state: MenuState) {
-        _speciesMenuState.value = state
-    }
-
-    fun addSpeciesValue(species: String) {
-        _speciesValues.value = addList(species, _speciesValues.value!!)
-    }
-    fun removeSpeciesValue(species: String) {
-        _speciesValues.value = removeList(species, _speciesValues.value!!)
-    }
-
-    private val _ratingMenuState: MutableLiveData<MenuState> = MutableLiveData(null)
-    val ratingMenuState: LiveData<MenuState> = _ratingMenuState
-
-    private val _ratingRadioBtnState: MutableLiveData<MutableList<Boolean>> = MutableLiveData(MutableList(5) {false})
-    val ratingRadioBtnState: LiveData<MutableList<Boolean>> = _ratingRadioBtnState
-
-    val selectedRatingText: LiveData<String> = _ratingRadioBtnState.map { list ->
-        buildSelectedText(list) { index ->  formatIndex(index) }
-    }
-
     fun setRatingMenuState(state: MenuState) {
         _ratingMenuState.value = state
     }
-    fun setRatingRadioBtnState(selectedIndex: Int) {
-        _ratingRadioBtnState.value = updateBtnStateList(selectedIndex, _ratingRadioBtnState.value!!)
-    }
-
-
-    private fun addList(value: String, currentValues: List<String>): List<String> {
-        val result = mutableListOf<String>()
-        result.addAll(currentValues)
-        result.add(value)
-        return result
-    }
-
-    private fun removeList(value: String, currentValue: List<String>): List<String> {
-        val result = mutableListOf<String>()
-        result.addAll(currentValue)
-        result.remove(value)
-        return result
-    }
-
+    // 他に開いているメニューがあれば閉じる処理
     fun updateMenuState(clickedView: View, context: Activity) {
         if (notExistCurrentOpenedView()) {
             currentOpenViewTag = clickedView.tag.toString()
@@ -200,6 +85,114 @@ class BeanFilterViewModel : BaseFilterViewModel() {
         }
     }
 
+    // 絞り込み要素 管理
+    private val _countryValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val countryValues: LiveData<List<String>> = _countryValues
+
+    private val _farmValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val farmValues: LiveData<List<String>> = _farmValues
+
+    private val _districtValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val districtValues: LiveData<List<String>> = _districtValues
+
+    private val _storeValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val storeValues: LiveData<List<String>> = _storeValues
+
+    private val _speciesValues: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val speciesValues: LiveData<List<String>> = _speciesValues
+
+    fun addCountryValue(country: String) {
+        _countryValues.value = addList(country, _countryValues.value!!)
+    }
+    fun removeCountryValue(country: String) {
+        _countryValues.value = removeList(country, _countryValues.value!!)
+    }
+    fun addFarmValue(farm: String) {
+        _farmValues.value = addList(farm, _farmValues.value!!)
+    }
+    fun removeFarmValue(farm: String) {
+        _farmValues.value = removeList(farm, _farmValues.value!!)
+    }
+    fun addDistrictValue(district: String) {
+        _districtValues.value = addList(district, _districtValues.value!!)
+    }
+    fun removeDistrictValue(district: String) {
+        _districtValues.value = removeList(district, _districtValues.value!!)
+    }
+    fun addStoreValue(store: String) {
+        _storeValues.value = addList(store, _storeValues.value!!)
+    }
+    fun removeStoreValue(store: String) {
+        _storeValues.value = removeList(store, _storeValues.value!!)
+    }
+    fun addSpeciesValue(species: String) {
+        _speciesValues.value = addList(species, _speciesValues.value!!)
+    }
+    fun removeSpeciesValue(species: String) {
+        _speciesValues.value = removeList(species, _speciesValues.value!!)
+    }
+    // 共通処理
+    private fun addList(value: String, currentValues: List<String>): List<String> {
+        val result = mutableListOf<String>()
+        result.addAll(currentValues)
+        result.add(value)
+        return result
+    }
+    private fun removeList(value: String, currentValue: List<String>): List<String> {
+        val result = mutableListOf<String>()
+        result.addAll(currentValue)
+        result.remove(value)
+        return result
+    }
+
+    // ラジオボタンの状態管理( 選択されたvalue 管理)
+    private val _ratingRadioBtnState: MutableLiveData<MutableList<Boolean>> = MutableLiveData(MutableList(5) {false})
+    val ratingRadioBtnState: LiveData<MutableList<Boolean>> = _ratingRadioBtnState
+
+    private val _processBtnStateList: MutableLiveData<MutableList<Boolean>> = MutableLiveData(MutableList(Constants.processList.size) { false })
+    val processBtnStateList: LiveData<MutableList<Boolean>> = _processBtnStateList
+
+    fun setProcessBtnState(selectedIndex: Int) {
+        _processBtnStateList.value = updateBtnStateList(selectedIndex, _processBtnStateList.value!!)
+    }
+    fun setRatingRadioBtnState(selectedIndex: Int) {
+        _ratingRadioBtnState.value = updateBtnStateList(selectedIndex, _ratingRadioBtnState.value!!)
+    }
+
+    // 絞り込み要素表示テキスト 管理
+    val inputCountriesText: LiveData<String> = _countryValues.map { list ->
+         if (list.isEmpty())  return@map ""
+
+        return@map list.joinToString(", ")
+    }
+    val inputFarmText: LiveData<String> = _farmValues.map { list ->
+        if (list.isEmpty())  return@map ""
+
+        return@map list.joinToString(", ")
+    }
+    val inputDistrictText: LiveData<String> = _districtValues.map { list ->
+         if (list.isEmpty())  return@map ""
+
+        return@map list.joinToString(", ")
+    }
+    val inputStoreText: LiveData<String> = _storeValues.map { list ->
+         if (list.isEmpty())  return@map ""
+
+        return@map list.joinToString(", ")
+    }
+    val selectedProcessText: LiveData<String> = _processBtnStateList.map { list ->
+        buildSelectedText(list) { index -> "${Constants.processList[index]},  " }
+    }
+    val inputSpeciesText: LiveData<String> = _speciesValues.map { list ->
+        if (list.isEmpty())  return@map ""
+
+        return@map list.joinToString(", ")
+    }
+    val selectedRatingText: LiveData<String> = _ratingRadioBtnState.map { list ->
+        buildSelectedText(list) { index ->  formatIndex(index) }
+    }
+
+    // filterManagerにデータをセット
     fun setUpFilteringManager(filterManager: BeanFilterManager) {
         filterManager.resetList()
 
