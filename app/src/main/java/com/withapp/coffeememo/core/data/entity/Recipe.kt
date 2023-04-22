5b4503957b31a4d62485e2ddea2a430e1683c73e
sin0713 <narisin1124@gmail.com>
2023-04-22 20:46:55 +0900

    ライブラリ最新バージョンに更新

--- app/src/main/java/com/withapp/coffeememo/core/data/entity/Recipe.kt
+++ app/src/main/java/com/withapp/coffeememo/core/data/entity/Recipe.kt
@@ -3,7 +3,7 @@ package com.withapp.coffeememo.core.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.ForeignKey.CASCADE
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 
 
