07ea7f5277b730f3d3f242e93fa4c08e97c30df5
sin0713 <narisin1124@gmail.com>
2024-06-23 23:59:45 +0900

    [Delete] FavoriteRecipePresenter

--- app/src/main/java/com/withapp/coffeememo/favorite/recipe/domain/interactor/GetFavoriteRecipeInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/favorite/recipe/domain/interactor/GetFavoriteRecipeInteractor.kt
@@ -1,14 +1,16 @@
 package com.withapp.coffeememo.favorite.recipe.domain.interactor
 
-import com.withapp.coffeememo.core.data.entity.Recipe
 import com.withapp.coffeememo.domain.repository.RecipeRepository
 import com.withapp.coffeememo.favorite.recipe.domain.use_case.GetFavoriteRecipeUseCase
+import com.withapp.coffeememo.favorite.recipe.presentation.mapper.FavoriteRecipeModelMapper
+import com.withapp.coffeememo.favorite.recipe.presentation.model.FavoriteRecipeModel
 import javax.inject.Inject
 
 class GetFavoriteRecipeInteractor @Inject constructor(
-    private val recipeRepo: RecipeRepository
+    private val recipeRepo: RecipeRepository,
+    private val mapper: FavoriteRecipeModelMapper
 ) : GetFavoriteRecipeUseCase {
-    override suspend fun handle(): List<Recipe> {
-        return recipeRepo.getFavoriteRecipe()
+    override suspend fun handle(): List<FavoriteRecipeModel> {
+        return recipeRepo.getFavoriteRecipe().map(mapper::execute)
     }
 }
--- app/src/main/java/com/withapp/coffeememo/favorite/recipe/presentation/presenter/FavoriteRecipePresenter.kt
+++
@@ -1,8 +0,0 @@
-package com.withapp.coffeememo.favorite.recipe.presentation.presenter
-
-import com.withapp.coffeememo.core.data.entity.Recipe
-import com.withapp.coffeememo.favorite.recipe.presentation.model.FavoriteRecipeModel
-
-interface FavoriteRecipePresenter {
-    fun presentFavoriteRecipe(recipes: List<Recipe>): List<FavoriteRecipeModel>
-}
