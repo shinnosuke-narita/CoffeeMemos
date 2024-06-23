643c47df119cc5063a6bbc65367a0cfe03304ffb
sin0713 <narisin1124@gmail.com>
2024-06-23 15:46:24 +0900

    [Add] BeanRepository

---
+++ app/src/main/java/com/withapp/coffeememo/domain/repository/TasteRepository.kt
@@ -0,0 +1,18 @@
+package com.withapp.coffeememo.domain.repository
+
+import com.withapp.coffeememo.core.data.entity.Taste
+import kotlinx.coroutines.flow.Flow
+
+interface TasteRepository {
+    suspend fun insert(taste: Taste)
+
+    suspend fun updateTaste(taste: Taste)
+
+    fun getAll(): Flow<List<Taste>>
+
+    suspend fun getTasteByRecipeId(recipeId: Long): Taste
+
+    suspend fun getTasteIdByRecipeId(recipeId: Long): Long
+
+    suspend fun getTasteById(id: Long): Taste
+}
