527432d94d0fa811c7a2d333ec197500aebb297e
sin0713 <narisin1124@gmail.com>
2024-06-24 07:33:09 +0900

    FreeWordSearchIterator presenter削除

--- app/src/main/java/com/withapp/coffeememo/search/recipe/domain/interacotr/FreeWordSearchIterator.kt
+++ app/src/main/java/com/withapp/coffeememo/search/recipe/domain/interacotr/FreeWordSearchIterator.kt
@@ -3,19 +3,14 @@ package com.withapp.coffeememo.search.recipe.domain.interacotr
 import com.withapp.coffeememo.domain.repository.RecipeRepository
 import com.withapp.coffeememo.search.recipe.data.mapper.SearchRecipeModelMapper
 import com.withapp.coffeememo.search.recipe.domain.model.SearchRecipeModel
-import com.withapp.coffeememo.search.recipe.domain.presenter.SearchRecipePresenter
 import com.withapp.coffeememo.search.recipe.domain.use_case.FreeWordSearchUseCase
 import javax.inject.Inject
 
 class FreeWordSearchIterator @Inject constructor(
     private val recipeRepo: RecipeRepository,
-    private val mapper: SearchRecipeModelMapper,
-    private val freeWordSearchPresenter: SearchRecipePresenter
+    private val mapper: SearchRecipeModelMapper
 ) : FreeWordSearchUseCase {
     override suspend fun handle(freeWord: String): List<SearchRecipeModel> {
-        val originRecipeList: List<SearchRecipeModel> =
-            recipeRepo.getRecipeWithTasteByKeyword(freeWord).map(mapper::execute)
-
-        return freeWordSearchPresenter.presentFreeWordSearchRes(originRecipeList)
+        return recipeRepo.getRecipeWithTasteByKeyword(freeWord).map(mapper::execute)
     }
 }
