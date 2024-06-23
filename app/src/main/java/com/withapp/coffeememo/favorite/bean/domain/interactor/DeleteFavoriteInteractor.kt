7f2e806d5965721048f54e63fa3ed5942e3e819d
sin0713 <narisin1124@gmail.com>
2024-06-23 16:31:35 +0900

    [Fix] DeleteFavoriteInteractor StorageRepo → BeanRepo

--- app/src/main/java/com/withapp/coffeememo/favorite/bean/domain/interactor/DeleteFavoriteInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/favorite/bean/domain/interactor/DeleteFavoriteInteractor.kt
@@ -1,15 +1,13 @@
 package com.withapp.coffeememo.favorite.bean.domain.interactor
 
-import com.withapp.coffeememo.favorite.bean.domain.repository.StorageRepository
+import com.withapp.coffeememo.domain.repository.BeanRepository
 import com.withapp.coffeememo.favorite.bean.domain.use_case.DeleteFavoriteUseCase
 import javax.inject.Inject
 
-class DeleteFavoriteInteractor @Inject constructor()
-    : DeleteFavoriteUseCase {
-    @Inject
-    lateinit var repository: StorageRepository
-
+class DeleteFavoriteInteractor @Inject constructor(
+    private val beanRepo: BeanRepository
+): DeleteFavoriteUseCase {
     override suspend fun handle(beanId: Long) {
-        repository.deleteFavoriteBean(beanId)
+        beanRepo.deleteFavoriteBean(beanId)
     }
 }
