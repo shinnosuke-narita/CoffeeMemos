c037a417d7f1490e40f84c41231cfb3b19ba4398
sin0713 <narisin1124@gmail.com>
2024-06-23 17:52:58 +0900

    [Fix] SearchRecipeDiskRepo -> RecipeRepo

--- app/src/main/java/com/withapp/coffeememo/search/recipe/domain/iterator/GetAllRecipeIterator.kt
+++ app/src/main/java/com/withapp/coffeememo/search/recipe/domain/iterator/GetAllRecipeIterator.kt
@@ -1,20 +1,19 @@
 package com.withapp.coffeememo.search.recipe.domain.iterator
 
+import com.withapp.coffeememo.domain.repository.RecipeRepository
+import com.withapp.coffeememo.search.recipe.data.mapper.SearchRecipeModelMapper
 import com.withapp.coffeememo.search.recipe.domain.model.SearchRecipeModel
 import com.withapp.coffeememo.search.recipe.domain.presenter.SearchRecipePresenter
-import com.withapp.coffeememo.search.recipe.domain.repository.SearchRecipeDiskRepository
 import com.withapp.coffeememo.search.recipe.domain.use_case.GetAllRecipeUseCase
 import javax.inject.Inject
 
-class GetAllRecipeIterator @Inject constructor()
-    : GetAllRecipeUseCase {
-    @Inject
-    lateinit var searchRecipeRepository: SearchRecipeDiskRepository
-    @Inject
-    lateinit var searchRecipePresenter: SearchRecipePresenter
-
+class GetAllRecipeIterator @Inject constructor(
+    private val recipeRepo: RecipeRepository,
+    private val mapper: SearchRecipeModelMapper,
+    private val searchRecipePresenter: SearchRecipePresenter
+) : GetAllRecipeUseCase {
     override suspend fun getAllRecipe(): List<SearchRecipeModel> {
-       val recipes = searchRecipeRepository.getAllRecipe()
+       val recipes = recipeRepo.getRecipeWithTaste().map(mapper::execute)
         return searchRecipePresenter.presentAllRecipes(recipes)
     }
 }
