61bfa918dec122630d8aa19fe25cc73b69f137a7
sin0713 <narisin1124@gmail.com>
2024-06-23 14:53:19 +0900

    Add RecipeRepository

---
+++ app/src/main/java/com/withapp/coffeememo/domain/repository/RecipeRepository.kt
@@ -0,0 +1,41 @@
+package com.withapp.coffeememo.domain.repository
+
+import com.withapp.coffeememo.core.data.entity.Recipe
+import com.withapp.coffeememo.core.data.entity.RecipeWithTaste
+import kotlinx.coroutines.flow.Flow
+
+interface RecipeRepository {
+    suspend fun insert(recipe: Recipe)
+
+    suspend fun insertAll(recipes: List<Recipe>)
+
+    suspend fun update(recipe: Recipe)
+
+    suspend fun updateFavoriteByRecipeId(
+        id: Long,
+        favoriteFlag: Boolean)
+
+    suspend fun updateCountryById(
+        id: Long,
+        country: String)
+
+    suspend fun deleteById(id: Long)
+
+    suspend fun getNewestRecipeId(): Long
+
+    suspend fun getRecipeById(id: Long): Recipe
+
+    suspend fun getRecipeIdsByBeanId(beanId: Long): List<Long>
+
+    fun getTotalCount(): Int
+
+    fun getRecipeWithTasteByFlow(): Flow<List<RecipeWithTaste>>
+
+    suspend fun getRecipeWithTaste(): List<RecipeWithTaste>
+
+    suspend fun getRecipeWithTasteByKeyword(
+        keyword: String
+    ): List<RecipeWithTaste>
+
+    suspend fun getFavoriteRecipe(): List<Recipe>
+}
