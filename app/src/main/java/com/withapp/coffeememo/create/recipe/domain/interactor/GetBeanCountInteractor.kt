a2ad39841c604f873307861e418e859cb06cb57c
sin0713 <narisin1124@gmail.com>
2024-06-23 16:15:38 +0900

    [Fix] GetBeanCountInteractor repo変更 StorageRepository → BeanRepository

--- app/src/main/java/com/withapp/coffeememo/create/recipe/domain/interactor/GetBeanCountInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/create/recipe/domain/interactor/GetBeanCountInteractor.kt
@@ -1,15 +1,13 @@
 package com.withapp.coffeememo.create.recipe.domain.interactor
 
-import com.withapp.coffeememo.create.recipe.domain.repository.StorageRepository
 import com.withapp.coffeememo.create.recipe.domain.use_case.GetBeanCountUseCase
+import com.withapp.coffeememo.domain.repository.BeanRepository
 import javax.inject.Inject
 
-class GetBeanCountInteractor @Inject constructor()
-    : GetBeanCountUseCase {
-    @Inject
-    lateinit var repository: StorageRepository
-
+class GetBeanCountInteractor @Inject constructor(
+    private val beanRepo: BeanRepository
+) : GetBeanCountUseCase {
     override suspend fun handle(): Int {
-        return repository.getBeanCount()
+        return beanRepo.getBeanCount()
     }
 }
