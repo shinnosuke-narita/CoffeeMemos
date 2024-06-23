d8351aea93e30a6ab1a70b4bce67c36df2d4ee77
sin0713 <narisin1124@gmail.com>
2024-06-23 17:49:57 +0900

    [Fix] SearchRecipeDiskRepo -> RecipeRepo

--- app/src/main/java/com/withapp/coffeememo/search/recipe/domain/iterator/FreeWordSearchIterator.kt
+++ app/src/main/java/com/withapp/coffeememo/search/recipe/domain/iterator/FreeWordSearchIterator.kt
@@ -1,20 +1,20 @@
 package com.withapp.coffeememo.search.recipe.domain.iterator
 
+import com.withapp.coffeememo.domain.repository.RecipeRepository
+import com.withapp.coffeememo.search.recipe.data.mapper.SearchRecipeModelMapper
 import com.withapp.coffeememo.search.recipe.domain.model.SearchRecipeModel
 import com.withapp.coffeememo.search.recipe.domain.presenter.SearchRecipePresenter
-import com.withapp.coffeememo.search.recipe.domain.repository.SearchRecipeDiskRepository
 import com.withapp.coffeememo.search.recipe.domain.use_case.FreeWordSearchUseCase
 import javax.inject.Inject
 
-class FreeWordSearchIterator @Inject constructor()
-    : FreeWordSearchUseCase {
-    @Inject
-    lateinit var freeWordSearchRepository: SearchRecipeDiskRepository
-    @Inject
-    lateinit var freeWordSearchPresenter: SearchRecipePresenter
-
+class FreeWordSearchIterator @Inject constructor(
+    private val recipeRepo: RecipeRepository,
+    private val mapper: SearchRecipeModelMapper,
+    private val freeWordSearchPresenter: SearchRecipePresenter
+) : FreeWordSearchUseCase {
     override suspend fun handle(freeWord: String): List<SearchRecipeModel> {
-        val originRecipeList: List<SearchRecipeModel> = freeWordSearchRepository.searchRecipeByFreeWord(freeWord)
+        val originRecipeList: List<SearchRecipeModel> =
+            recipeRepo.getRecipeWithTasteByKeyword(freeWord).map(mapper::execute)
 
         return freeWordSearchPresenter.presentFreeWordSearchRes(originRecipeList)
     }
