801400cb1b4af2644c5eb16ee1869b240ac63b73
sin0713 <narisin1124@gmail.com>
2024-06-23 16:35:51 +0900

    [Fix] GetFavoriteBeanInteractor StorageRepo -> BeanRepo

--- app/src/main/java/com/withapp/coffeememo/favorite/bean/domain/interactor/GetFavoriteBeanInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/favorite/bean/domain/interactor/GetFavoriteBeanInteractor.kt
@@ -1,16 +1,14 @@
 package com.withapp.coffeememo.favorite.bean.domain.interactor
 
 import com.withapp.coffeememo.core.data.entity.Bean
-import com.withapp.coffeememo.favorite.bean.domain.repository.StorageRepository
+import com.withapp.coffeememo.domain.repository.BeanRepository
 import com.withapp.coffeememo.favorite.bean.domain.use_case.GetFavoriteBeanUseCase
 import javax.inject.Inject
 
-class GetFavoriteBeanInteractor @Inject constructor()
-    : GetFavoriteBeanUseCase {
-    @Inject
-    lateinit var repository: StorageRepository
-
+class GetFavoriteBeanInteractor @Inject constructor(
+    private val beanRepo: BeanRepository
+) : GetFavoriteBeanUseCase {
     override suspend fun handle(): List<Bean> {
-        return repository.getFavoriteBeans()
+        return beanRepo.getFavoriteBean()
     }
 }
