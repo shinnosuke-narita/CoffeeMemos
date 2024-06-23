1b3ffd396d2af8554bc817571c8d86c5ecf8344a
sin0713 <narisin1124@gmail.com>
2024-06-23 17:38:24 +0900

    [Delete] StorageRepo

--- app/src/main/java/com/withapp/coffeememo/favorite/recipe/domain/repository/StorageRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.withapp.coffeememo.favorite.recipe.domain.repository
-
-import com.withapp.coffeememo.core.data.entity.Recipe
-
-interface StorageRepository {
-    suspend fun getFavoriteRecipe(): List<Recipe>
-    suspend fun deleteFavorite(recipeId: Long)
-}
