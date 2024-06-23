b038b53b3bb7558ae5bc4d1469b762b7b7d3e5b9
sin0713 <narisin1124@gmail.com>
2024-06-24 07:31:43 +0900

    [Fix] delete presenter from GetAllRecipeInteractor

--- app/src/main/java/com/withapp/coffeememo/search/recipe/domain/interacotr/GetAllRecipeInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/search/recipe/domain/interacotr/GetAllRecipeInteractor.kt
@@ -3,17 +3,14 @@ package com.withapp.coffeememo.search.recipe.domain.interacotr
 import com.withapp.coffeememo.domain.repository.RecipeRepository
 import com.withapp.coffeememo.search.recipe.data.mapper.SearchRecipeModelMapper
 import com.withapp.coffeememo.search.recipe.domain.model.SearchRecipeModel
-import com.withapp.coffeememo.search.recipe.domain.presenter.SearchRecipePresenter
 import com.withapp.coffeememo.search.recipe.domain.use_case.GetAllRecipeUseCase
 import javax.inject.Inject
 
 class GetAllRecipeInteractor @Inject constructor(
     private val recipeRepo: RecipeRepository,
-    private val mapper: SearchRecipeModelMapper,
-    private val searchRecipePresenter: SearchRecipePresenter
+    private val mapper: SearchRecipeModelMapper
 ) : GetAllRecipeUseCase {
     override suspend fun getAllRecipe(): List<SearchRecipeModel> {
-       val recipes = recipeRepo.getRecipeWithTaste().map(mapper::execute)
-        return searchRecipePresenter.presentAllRecipes(recipes)
+        return recipeRepo.getRecipeWithTaste().map(mapper::execute)
     }
 }
