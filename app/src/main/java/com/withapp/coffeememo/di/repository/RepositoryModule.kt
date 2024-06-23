928780845114b36656856799279196aee1e7be07
sin0713 <narisin1124@gmail.com>
2024-06-23 15:41:41 +0900

    [Add] BeanRepository DIセットアップ

--- app/src/main/java/com/withapp/coffeememo/di/repository/RepositoryModule.kt
+++ app/src/main/java/com/withapp/coffeememo/di/repository/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.withapp.coffeememo.di.repository
 
+import com.withapp.coffeememo.data.bean.BeanRepositoryImpl
 import com.withapp.coffeememo.data.recipe.RecipeRepositoryImpl
+import com.withapp.coffeememo.domain.repository.BeanRepository
 import com.withapp.coffeememo.domain.repository.RecipeRepository
 import dagger.Binds
 import dagger.Module
@@ -15,4 +17,9 @@ abstract class RepositoryModule {
     abstract fun bindRecipeRepository(
         recipeRepositoryImpl: RecipeRepositoryImpl
     ): RecipeRepository
+
+    @Binds
+    abstract fun bindBeanRepository(
+        beanRepositoryImpl: BeanRepositoryImpl
+    ): BeanRepository
 }
