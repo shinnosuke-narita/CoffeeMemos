35c85a562bc26a4d86fcd9697ff6bc2dcc12cc02
sin0713 <narisin1124@gmail.com>
2024-06-23 17:13:31 +0900

    [Fix] FilterBeanIterator SearchBeanDiskRepo -> BeanRepo

--- app/src/main/java/com/withapp/coffeememo/search/bean/domain/iterator/FilterBeanIterator.kt
+++ app/src/main/java/com/withapp/coffeememo/search/bean/domain/iterator/FilterBeanIterator.kt
@@ -1,24 +1,22 @@
 package com.withapp.coffeememo.search.bean.domain.iterator
 
+import com.withapp.coffeememo.domain.repository.BeanRepository
 import com.withapp.coffeememo.search.bean.domain.model.FilterBeanInputData
 import com.withapp.coffeememo.search.bean.domain.model.SearchBeanModel
 import com.withapp.coffeememo.search.bean.domain.presenter.SearchBeanPresenter
-import com.withapp.coffeememo.search.bean.domain.repository.SearchBeanDiskRepository
 import com.withapp.coffeememo.search.bean.domain.use_case.FilterBeanUseCase
 import javax.inject.Inject
 
-class FilterBeanIterator @Inject constructor() : FilterBeanUseCase {
-    @Inject
-    lateinit var searchBeanDiskRepository: SearchBeanDiskRepository
-    @Inject
-    lateinit var searchBeanPresenter: SearchBeanPresenter
-
+class FilterBeanIterator @Inject constructor(
+    private val beanRepo: BeanRepository,
+    private val searchBeanPresenter: SearchBeanPresenter
+) : FilterBeanUseCase {
     override suspend fun filterBean(inputData: FilterBeanInputData): List<SearchBeanModel> {
         val beans: List<SearchBeanModel> =
             if (inputData.keyWord.isNotEmpty()) {
-                 searchBeanDiskRepository.searchBeanByFreeWord(inputData.keyWord)
+                 beanRepo.getSearchBeanModelByKeyword(inputData.keyWord)
             } else {
-                 searchBeanDiskRepository.getAllBean()
+                 beanRepo.getSearchBeanModel()
             }
 
         if (beans.isEmpty()) {
