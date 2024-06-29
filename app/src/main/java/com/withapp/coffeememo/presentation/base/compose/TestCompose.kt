21346b93c27b35314d41a4cda2a79d52a4b398ad
sin0713 <narisin1124@gmail.com>
2024-06-29 09:14:41 +0900

    [Add] theme color

--- app/src/main/java/com/withapp/coffeememo/presentation/base/compose/TestCompose.kt
+++ app/src/main/java/com/withapp/coffeememo/presentation/base/compose/TestCompose.kt
@@ -1,14 +1,21 @@
 package com.withapp.coffeememo.presentation.base.compose
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.withapp.coffeememo.presentation.ui.theme.AppTheme
 
 
 @Composable
 fun HelloComposable(modifier: Modifier = Modifier) {
-    Text(text = "Hello Composable")
+    AppTheme {
+        Button(
+            onClick = { /* ... */ },
+            modifier = modifier
+        ) { Text(text = "hello world") }
+    }
 }
 
 @Preview
