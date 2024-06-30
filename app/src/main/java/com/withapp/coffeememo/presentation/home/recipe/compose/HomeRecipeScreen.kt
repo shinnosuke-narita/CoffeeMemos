cbf99e2be3ed1624c365a2c6c048fef5b2d090b4
sin0713 <narisin1124@gmail.com>
2024-06-30 19:58:55 +0900

    [Change] レシピホーム画面 Composeリプレイス

--- app/src/main/java/com/withapp/coffeememo/presentation/home/recipe/compose/HomeRecipeScreen.kt
+++ app/src/main/java/com/withapp/coffeememo/presentation/home/recipe/compose/HomeRecipeScreen.kt
@@ -1,9 +1,100 @@
 package com.withapp.coffeememo.presentation.home.recipe.compose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.withapp.coffeememo.R
+import com.withapp.coffeememo.presentation.home.recipe.compose.components.HomeRecipeCard
+import com.withapp.coffeememo.presentation.home.recipe.compose.components.HomeRecipeStatusCard
+import com.withapp.coffeememo.presentation.home.recipe.model.HomeRecipeCardData
+import com.withapp.coffeememo.presentation.home.recipe.view_model.HomeRecipeViewModel
 
 @Composable
-fun HomeRecipeScreen(modifier: Modifier = Modifier) {
+fun HomeRecipeScreen(
+    viewModel: HomeRecipeViewModel = viewModel(),
+    onCreateButton: () -> Unit
+) {
+    val newRecipes by viewModel.newRecipes.observeAsState()
+    val favoriteRecipes by viewModel.favoriteRecipes.observeAsState()
+    val highRatingRecipes by viewModel.highRatingRecipes.observeAsState()
+    val totalCounts by viewModel.totalRecipeCount.observeAsState()
+    val favoriteCounts by viewModel.favoriteRecipeCount.observeAsState()
+    val todayRecipeCounts by viewModel.todayRecipeCount.observeAsState()
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_small)))
+        HomeRecipeStatusCard(
+            allCounts = (totalCounts ?: 0).toString(),
+            favoriteCounts = (favoriteCounts ?: 0).toString(),
+            todayCounts = (todayRecipeCounts ?: 0).toString(),
+            onCreateButton = onCreateButton
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_medium)))
+        Text(
+            text = stringResource(id = R.string.new_recipe_Header),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_small)))
+        CardList(recipes = newRecipes)
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_medium)))
+        Text(
+            text = stringResource(id = R.string.favorite_recipe_header),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_small)))
+        CardList(recipes = favoriteRecipes)
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_medium)))
+        Text(
+            text = stringResource(id = R.string.high_rating_header),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Spacer(Modifier.size(dimensionResource(id = R.dimen.margin_small)))
+        CardList(recipes = highRatingRecipes)
+    }
+}
+
+@Composable
+fun CardList(recipes: List<HomeRecipeCardData>?) {
+    recipes?.let {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(
+                dimensionResource(id = R.dimen.margin_small)
+            )
+        ) {
+            items(it) { recipe ->
+                HomeRecipeCard(
+                    recipe = recipe,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeRecipeScreenPreview() {
+    HomeRecipeScreen(onCreateButton = {})
 }
--- app/src/main/java/com/withapp/coffeememo/presentation/home/recipe/fragment/HomeRecipeFragment.kt
+++ app/src/main/java/com/withapp/coffeememo/presentation/home/recipe/fragment/HomeRecipeFragment.kt
@@ -12,7 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.withapp.coffeememo.R
 import com.withapp.coffeememo.databinding.FragmentHomeRecipeBinding
+import com.withapp.coffeememo.presentation.components.ThemeComposable
 import com.withapp.coffeememo.presentation.home.recipe.adapter.RecipeAdapter
+import com.withapp.coffeememo.presentation.home.recipe.compose.HomeRecipeScreen
 import com.withapp.coffeememo.presentation.home.recipe.view_model.HomeRecipeViewModel
 import com.withapp.coffeememo.presentation.utilities.SnackBarUtil
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,64 +40,76 @@ class HomeRecipeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeRecipeBinding
             .inflate(inflater, container, false)
+            .apply {
+                composeView.setContent {
+                    ThemeComposable {
+                        HomeRecipeScreen(
+                            onCreateButton = {
+                                findNavController().navigate(R.id.newRecipeFragment)
+                            }
+                        )
+                    }
+                }
+            }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // RecyclerView セットアップ
-        setUpRecyclerView()
-
-        // 今日作成レシピ数
-        viewModel.todayRecipeCount.observe(
-            viewLifecycleOwner) { todayRecipeNum ->
-            binding.todayRecipeNum.text = todayRecipeNum.toString()
-        }
-
-        // お気に入りレシピ数
-        viewModel.favoriteRecipeCount.observe(
-            viewLifecycleOwner) { favoriteRecipeNum ->
-            binding.favoriteRecipeNum.text = favoriteRecipeNum
-        }
-
-        // 監視処理
-        viewModel.newRecipes.observe(
-            viewLifecycleOwner) { list ->
-            _newRecipeAdapter.submitList(list)
-        }
-
-        viewModel.favoriteRecipes.observe(
-            viewLifecycleOwner) { favoriteList ->
-            _favoriteRecipeAdapter.submitList(favoriteList)
-        }
-
-        viewModel.highRatingRecipes.observe(
-            viewLifecycleOwner) { highRatingList ->
-            _highRatingAdapter.submitList(highRatingList)
-        }
-
-
-        // レシピの登録数 監視処理
-        viewModel.totalRecipeCount.observe(
-            viewLifecycleOwner) { recipeNum ->
-            binding.recipeTotalNum.text = recipeNum.toString()
-
-            if (recipeNum == 0) {
-                binding.newRecipeHeader.visibility = View.GONE
-                binding.highRatingRecipeHeader.visibility = View.GONE
-                binding.favoriteRecipeHeader.visibility = View.GONE
-            } else {
-                binding.newRecipeHeader.visibility = View.VISIBLE
-                binding.highRatingRecipeHeader.visibility = View.VISIBLE
-                binding.favoriteRecipeHeader.visibility = View.VISIBLE
-            }
-        }
-
-        // レシピ新規作成画面へ遷移
-        binding.createRecipeBtn.setOnClickListener {
-            findNavController().navigate(R.id.newRecipeFragment)
-        }
+//        // RecyclerView セットアップ
+//        setUpRecyclerView()
+//
+//        // 今日作成レシピ数
+//        viewModel.todayRecipeCount.observe(
+//            viewLifecycleOwner) { todayRecipeNum ->
+//            binding.todayRecipeNum.text = todayRecipeNum.toString()
+//        }
+//
+//        // お気に入りレシピ数
+//        viewModel.favoriteRecipeCount.observe(
+//            viewLifecycleOwner) { favoriteRecipeNum ->
+//            binding.favoriteRecipeNum.text = favoriteRecipeNum
+//        }
+//
+//        // 監視処理
+//        viewModel.newRecipes.observe(
+//            viewLifecycleOwner) { list ->
+//            _newRecipeAdapter.submitList(list)
+//        }
+//
+//        viewModel.favoriteRecipes.observe(
+//            viewLifecycleOwner) { favoriteList ->
+//            _favoriteRecipeAdapter.submitList(favoriteList)
+//        }
+//
+//        viewModel.highRatingRecipes.observe(
+//            viewLifecycleOwner) { highRatingList ->
+//            _highRatingAdapter.submitList(highRatingList)
+//        }
+//
+//
+//        // レシピの登録数 監視処理
+//        viewModel.totalRecipeCount.observe(
+//            viewLifecycleOwner) { recipeNum ->
+//            binding.recipeTotalNum.text = recipeNum.toString()
+//
+//            if (recipeNum == 0) {
+//                binding.newRecipeHeader.visibility = View.GONE
+//                binding.highRatingRecipeHeader.visibility = View.GONE
+//                binding.favoriteRecipeHeader.visibility = View.GONE
+//            } else {
+//                binding.newRecipeHeader.visibility = View.VISIBLE
+//                binding.highRatingRecipeHeader.visibility = View.VISIBLE
+//                binding.favoriteRecipeHeader.visibility = View.VISIBLE
+//            }
+//        }
+//
+//        // レシピ新規作成画面へ遷移
+//        binding.createRecipeBtn.setOnClickListener {
+//            findNavController().navigate(R.id.newRecipeFragment)
+//        }
 
         // 豆ホーム画面へ遷移
         binding.goToBeanBtn.setOnClickListener {
@@ -136,44 +150,44 @@ class HomeRecipeFragment : Fragment() {
         _binding = null
     }
 
-    private fun setUpRecyclerView() {
-        _newRecipeAdapter = getHomeRecipeAdapter()
-        _favoriteRecipeAdapter = getHomeRecipeAdapter()
-        _highRatingAdapter = getHomeRecipeAdapter()
+//    private fun setUpRecyclerView() {
+//        _newRecipeAdapter = getHomeRecipeAdapter()
+//        _favoriteRecipeAdapter = getHomeRecipeAdapter()
+//        _highRatingAdapter = getHomeRecipeAdapter()
+//
+//        binding.newRecipeList.apply {
+//            adapter = _newRecipeAdapter
+//            layoutManager = getLinerLayoutManger()
+//        }
+//        binding.favoriteRecipeList.apply {
+//            adapter = _favoriteRecipeAdapter
+//            layoutManager = getLinerLayoutManger()
+//        }
+//        binding.highRatingRecipeList.apply {
+//            adapter = _highRatingAdapter
+//            layoutManager = getLinerLayoutManger()
+//        }
+//    }
 
-        binding.newRecipeList.apply {
-            adapter = _newRecipeAdapter
-            layoutManager = getLinerLayoutManger()
-        }
-        binding.favoriteRecipeList.apply {
-            adapter = _favoriteRecipeAdapter
-            layoutManager = getLinerLayoutManger()
-        }
-        binding.highRatingRecipeList.apply {
-            adapter = _highRatingAdapter
-            layoutManager = getLinerLayoutManger()
-        }
-    }
-
-    private fun getHomeRecipeAdapter(): RecipeAdapter {
-       return RecipeAdapter(
-           context = requireContext(),
-           onFavoriteClick = { recipe ->
-               viewModel.updateHomeData(
-                   recipe.recipeId,
-                   recipe.isFavorite)
-           },
-           onItemClick = { recipe ->
-               val showDetailAction =
-                   HomeRecipeFragmentDirections.showRecipeDetailAction()
-                       .apply {
-                           recipeId = recipe.recipeId
-                           beanId   = recipe.beanId
-                       }
-               findNavController().navigate(showDetailAction)
-           }
-       )
-    }
+//    private fun getHomeRecipeAdapter(): RecipeAdapter {
+//       return RecipeAdapter(
+//           context = requireContext(),
+//           onFavoriteClick = { recipe ->
+//               viewModel.updateHomeData(
+//                   recipe.recipeId,
+//                   recipe.isFavorite)
+//           },
+//           onItemClick = { recipe ->
+//               val showDetailAction =
+//                   HomeRecipeFragmentDirections.showRecipeDetailAction()
+//                       .apply {
+//                           recipeId = recipe.recipeId
+//                           beanId   = recipe.beanId
+//                       }
+//               findNavController().navigate(showDetailAction)
+//           }
+//       )
+//    }
 
     private fun getLinerLayoutManger(): LinearLayoutManager {
        return LinearLayoutManager(
