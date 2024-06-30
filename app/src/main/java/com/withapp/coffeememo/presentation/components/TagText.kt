0c732eda263e09c6811aeeb3eb3ee2c53375bac1
sin0713 <narisin1124@gmail.com>
2024-06-30 16:23:22 +0900

    [Add] TagText 追加

--- app/src/main/java/com/withapp/coffeememo/presentation/components/TagText.kt
+++ app/src/main/java/com/withapp/coffeememo/presentation/components/TagText.kt
@@ -1,2 +1,41 @@
 package com.withapp.coffeememo.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.withapp.coffeememo.R
+
+@Composable
+fun TagText(text: String) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.inversePrimary,
+                shape = MaterialTheme.shapes.large
+            )
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.inverseOnSurface
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TagTextPreview() {
+    ThemeComposable {
+        TagText("フルシティロースト")
+    }
+}
+
