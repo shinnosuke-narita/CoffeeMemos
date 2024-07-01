5212dc4d3e2e4c88ae8a9c2666fe0d4dba15b2d1
sin0713 <narisin1124@gmail.com>
2024-07-01 22:26:28 +0900

    [Add] margin padding borderwidth 追加

--- app/src/main/java/com/withapp/coffeememo/presentation/ui/theme/Space.kt
+++ app/src/main/java/com/withapp/coffeememo/presentation/ui/theme/Space.kt
@@ -5,11 +5,31 @@ import androidx.compose.ui.unit.dp
 
 internal data object CoffeeMemoAppDefaults {
     val IconSize: IconSize = IconSize()
-
+    val Margin: Margin = Margin()
+    val Padding: Padding = Padding()
+    val BorderWidth: BorderWidth = BorderWidth()
 }
 
 internal data class IconSize(
     val default: Dp = 24.dp,
     val medium: Dp = 40.dp,
     val large: Dp  = 48.dp
+)
+
+internal data class Margin(
+    val small: Dp = 8.dp,
+    val medium: Dp = 14.dp,
+    val large: Dp  = 20.dp,
+    val extraLarge: Dp = 32.dp
+)
+
+internal data class Padding(
+    val small: Dp = 8.dp,
+    val medium: Dp = 14.dp,
+    val large: Dp  = 20.dp,
+    val extraLarge: Dp = 32.dp
+)
+
+internal data class BorderWidth(
+    val default: Dp = 1.dp
 )
