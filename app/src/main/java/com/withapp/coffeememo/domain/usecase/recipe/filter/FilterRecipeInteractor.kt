e73cd6023a00c876b6f633d56ef590c0f22b1ab5
sin0713 <narisin1124@gmail.com>
2024-09-03 23:16:02 +0900

    [Refactor] FilterRecipeInteractor

--- app/src/main/java/com/withapp/coffeememo/domain/usecase/recipe/filter/FilterRecipeInteractor.kt
+++ app/src/main/java/com/withapp/coffeememo/domain/usecase/recipe/filter/FilterRecipeInteractor.kt
@@ -11,91 +11,27 @@ class FilterRecipeInteractor @Inject constructor(
     private val mapper: SearchRecipeModelMapper
 ) : FilterRecipeUseCase {
     override suspend fun filterRecipe(inputData: FilterRecipeInputData): List<SearchRecipeModel> {
-        val recipes: List<SearchRecipeModel> =
+        val recipes =
             if (inputData.keyWord.isNotEmpty()) {
-                 recipeRepo.getRecipeWithTasteByKeyword(inputData.keyWord).map(mapper::execute)
+                 recipeRepo.getRecipeWithTasteByKeyword(inputData.keyWord)
             } else {
-                 recipeRepo.getRecipeWithTaste().map(mapper::execute)
+                 recipeRepo.getRecipeWithTaste()
             }
 
-        if (recipes.isEmpty()) return recipes
-
-        // 酸味
-        var result = getMatchedRecipes(recipes, inputData.sour) {
-                recipe, value -> recipe.sour == value }
-        if (result.isEmpty())  return result
-
-        // 苦味
-        result = getMatchedRecipes(result, inputData.bitter) {
-            recipe, data -> recipe.bitter == data
-        }
-        if (result.isEmpty()) return result
-
-        // 甘味
-        result = getMatchedRecipes(result, inputData.sweet) {
-                recipe, data -> recipe.sweet == data
-        }
-        if (result.isEmpty()) return result
-
-        // 香り
-        result = getMatchedRecipes(result, inputData.flavor) {
-                recipe, data -> recipe.flavor == data
-        }
-        if (result.isEmpty()) return result
-
-        // コク
-        result = getMatchedRecipes(result, inputData.rich) {
-                recipe, data -> recipe.rich == data
-        }
-        if (result.isEmpty()) return result
-        // 評価
-        result = getMatchedRecipes(result, inputData.rating) {
-                recipe, data -> recipe.rating == data
-        }
-        if (result.isEmpty()) return result
-
-        // 焙煎度
-        result = getMatchedRecipes(result, inputData.roasts) {
-                recipe, data -> recipe.roast == data
-        }
-        if (result.isEmpty()) return result
-
-        // 粒度
-        result = getMatchedRecipes(result, inputData.grindSizes) {
-                recipe, data -> recipe.grindSize == data
-        }
-        if (result.isEmpty()) return result
-
-        // 原産地
-        result = getMatchedRecipes(result, inputData.countries) {
-                recipe, data -> recipe.country.contains(data)
-        }
-        if (result.isEmpty()) return result
-
-        // 抽出器具
-        result = getMatchedRecipes(result, inputData.tools) {
-                recipe, data -> recipe.tool.contains(data)
-        }
-
-        return result
+        return recipes.asSequence()
+            .map(mapper::execute)
+            .filter { recipe -> filteringPredicate(recipe.sour, inputData.sour) }
+            .filter { recipe -> filteringPredicate(recipe.bitter, inputData.bitter) }
+            .filter { recipe -> filteringPredicate(recipe.sweet, inputData.sweet) }
+            .filter { recipe -> filteringPredicate(recipe.flavor, inputData.flavor) }
+            .filter { recipe -> filteringPredicate(recipe.rich, inputData.rich) }
+            .filter { recipe -> filteringPredicate(recipe.rating, inputData.rating) }
+            .filter { recipe -> filteringPredicate(recipe.grindSize, inputData.grindSizes) }
+            .filter { recipe -> filteringPredicate(recipe.country, inputData.countries) }
+            .filter { recipe -> filteringPredicate(recipe.tool, inputData.tools) }
+            .toList()
     }
 
-    private fun <T> getMatchedRecipes(
-        recipes: List<SearchRecipeModel>,
-        filteringData: List<T>,
-        isMatch: (recipe: SearchRecipeModel, value: T) -> Boolean
-    ): List<SearchRecipeModel> {
-        if (filteringData.isEmpty()) return recipes
-
-        val res: MutableList<SearchRecipeModel> = mutableListOf()
-        for (recipe in recipes) {
-            for (value in filteringData) {
-                if (isMatch(recipe, value)) {
-                    res.add(recipe)
-                }
-            }
-        }
-
-        return res
-    }
+    private fun <T> filteringPredicate(target: T, filteringValue: List<T>): Boolean =
+        if (filteringValue.isEmpty()) true else filteringValue.contains(target)
 }
